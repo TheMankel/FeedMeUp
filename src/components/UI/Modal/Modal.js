07fc739d7f1127166b6ca1c6ec6399aae464cebb
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-08-31 10:54:17 +0200

    Added Modal

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,30 @@
+import { createPortal } from 'react-dom';
+import classes from './Modal.module.css';
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.querySelector('#overlays');
+
+function Modal(props) {
+  return (
+    <>
+      {createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement,
+      )}
+    </>
+  );
+}
+
+export default Modal;
