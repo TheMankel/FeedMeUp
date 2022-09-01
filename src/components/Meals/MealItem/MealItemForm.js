8fc40d31c4d388b3e57902a1a4387bfb0a0b2d03
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-01 18:14:42 +0200

    Created MealItemForm

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,1 +1,17 @@
+import React from 'react';
 
+import classes from './MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <div>
+        <label>Amount</label>
+        <input id={props.id} type='number' min='1' max='5' step='1'></input>
+      </div>
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
