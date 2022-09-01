6506a5c041d490ecc31bbbb0f9169a9b116922b3
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-01 18:19:41 +0200

    Update MealItemForm.js

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -7,7 +7,13 @@ const MealItemForm = (props) => {
     <form className={classes.form}>
       <div>
         <label>Amount</label>
-        <input id={props.id} type='number' min='1' max='5' step='1'></input>
+        <input
+          id={'amount_' + props.id}
+          type='number'
+          min='1'
+          max='5'
+          step='1'
+          defaultValue='1'></input>
       </div>
       <button>Add</button>
     </form>
