0d709de59b24a02ee9a0147ee5ae2e43d0f4c941
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-01 17:14:22 +0200

    Added some MealsSummary styling

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,17 +1,45 @@
+import LocationIcon from './Icons/LocationIcon';
+import FoodIcon from './Icons/FoodIcon';
+import ShoppingIcon from './Icons/ShoppingIcon';
 import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
       <h2>Delicious Food, Delivered To You</h2>
-      <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
-      </p>
+      <h3>It's as easy as this.</h3>
+      <div className={classes.steps}>
+        <div className={classes.step}>
+          <div className={classes.icon}>
+            <LocationIcon />
+            <span>1</span>
+          </div>
+          <div className={classes.description}>
+            <h4>Provide your location</h4>
+            <p>Fill out your address.</p>
+          </div>
+        </div>
+        <div className={classes.step}>
+          <div className={classes.icon}>
+            <FoodIcon />
+            <span>2</span>
+          </div>
+          <div className={classes.description}>
+            <h4>Choose your meal</h4>
+            <p>Browse through many restaurants and cuisines.</p>
+          </div>
+        </div>
+        <div className={classes.step}>
+          <div className={classes.icon}>
+            <ShoppingIcon />
+            <span>3</span>
+          </div>
+          <div className={classes.description}>
+            <h4>Pay and get your food</h4>
+            <p>Pay using various payment methods.</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
