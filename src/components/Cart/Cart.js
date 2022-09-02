95007364864ddd7da8733412d7e35199b748451a
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-02 19:32:58 +0200

    Added basic Cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Modal from '../UI/Modal/Modal';
+
+const Cart = (props) => {
+  return (
+    <>
+      <Modal onClose={props.onClose}>PRODUCTS</Modal>
+    </>
+  );
+};
+
+export default Cart;
