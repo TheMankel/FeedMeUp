d4e66c9a131a4641680c712ad269b0cb6f794f17
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-02 19:33:25 +0200

    Added show/hide cart modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+  const [cartVisible, setCartVisible] = useState(false);
+
+  const closeCartHandler = () => {
+    setCartVisible(false);
+  };
+
+  const openCartHandler = () => {
+    setCartVisible(true);
+  };
+
   return (
     <>
-      <Header />
+      {cartVisible && <Cart onClose={closeCartHandler} />}
+      <Header onShow={openCartHandler} />
       <Meals />
     </>
   );
