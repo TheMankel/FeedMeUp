73362d8fb1c9cbd62cdc7d46731ed0340f6a0ae0
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-03 22:57:37 +0200

    Added bump cart animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useRef } from 'react';
 import CartContext from '../../store/cart-context';
 
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
-  const cartCtx = useContext(CartContext);
-  const itemsAmount = cartCtx.items.reduce((previousValue, item) => {
+  const cartCtx = React.useContext(CartContext);
+  const { items } = cartCtx;
+
+  const itemsAmount = items.reduce((previousValue, item) => {
     return previousValue + item.amount;
   }, 0);
 
+  const formRef = useRef();
+
+  useEffect(() => {
+    if (items.length === 0) return;
+
+    formRef.current.classList.add(classes.bump);
+
+    const timer = setTimeout(() => {
+      formRef.current.classList.remove(classes.bump);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onShow}>
+    <button className={classes.button} onClick={props.onShow} ref={formRef}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
