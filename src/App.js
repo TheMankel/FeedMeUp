9cdda572db30f484d6add2529586b48958e63052
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-03 14:38:56 +0200

    Added CartProvider to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartVisible, setCartVisible] = useState(false);
@@ -15,11 +16,13 @@ function App() {
   };
 
   return (
-    <>
+    <CartProvider>
       {cartVisible && <Cart onClose={closeCartHandler} />}
       <Header onShow={openCartHandler} />
-      <Meals />
-    </>
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
