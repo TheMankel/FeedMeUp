8f7e5f9ba453cec12b9a15f928fb15cc8c246891
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-04 13:09:08 +0200

    Refactor

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,33 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Meal from '../Meal';
 
-import CartContext from '../../../store/cart-context';
-import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
-  const cartCtx = React.useContext(CartContext);
+  const [mealVisible, setMealVisible] = useState(false);
   const price = `$${props.price.toFixed(2)}`;
 
-  const addHandler = (amount) => {
-    cartCtx.addItem({
-      id: props.id,
-      name: props.name,
-      price: props.price,
-      amount: amount,
-    });
+  const openMealHandler = () => {
+    setMealVisible(true);
+  };
+
+  const closeMealHandler = () => {
+    setMealVisible(false);
   };
 
   return (
-    <li className={classes.meal}>
-      <div>
-        <h3>{props.name}</h3>
-        <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
-      </div>
-      <div>
-        <MealItemForm id={props.id} addHandler={addHandler} />
-      </div>
-    </li>
+    <>
+      {mealVisible && <Meal onClose={closeMealHandler} item={{ ...props }} />}
+      <li className={classes.meal} onClick={openMealHandler}>
+        <div className={classes.item}>
+          <h3>{props.name}</h3>
+          <div className={classes.description}>{props.description}</div>
+          <div className={classes.price}>{price}</div>
+        </div>
+        <div className={classes.image}>
+          <img src={props.image} alt={`Fresh and tasty ${props.name}`} />
+        </div>
+      </li>
+    </>
   );
 };
 
