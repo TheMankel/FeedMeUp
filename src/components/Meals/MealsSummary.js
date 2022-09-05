bf78161cd99df82717982a43e682f895d6d1c90a
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-05 09:45:02 +0200

    MealsSummary refactor

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -5,38 +5,40 @@ import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
   return (
-    <section className={classes.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
-      <h3>It's as easy as this.</h3>
-      <div className={classes.steps}>
-        <div className={classes.step}>
-          <div className={classes.icon}>
-            <LocationIcon />
-            <span>1</span>
+    <section className={classes.wrapper}>
+      <div className={classes.summary}>
+        <h2>Delicious Food, Delivered To You</h2>
+        <h3>It's as easy as this.</h3>
+        <div className={classes.steps}>
+          <div className={classes.step}>
+            <div className={classes.icon}>
+              <LocationIcon />
+              <span>1</span>
+            </div>
+            <div className={classes.description}>
+              <h4>Provide your location</h4>
+              <p>Fill out your address.</p>
+            </div>
           </div>
-          <div className={classes.description}>
-            <h4>Provide your location</h4>
-            <p>Fill out your address.</p>
+          <div className={classes.step}>
+            <div className={classes.icon}>
+              <FoodIcon />
+              <span>2</span>
+            </div>
+            <div className={classes.description}>
+              <h4>Choose your meal</h4>
+              <p>Browse through many restaurants and cuisines.</p>
+            </div>
           </div>
-        </div>
-        <div className={classes.step}>
-          <div className={classes.icon}>
-            <FoodIcon />
-            <span>2</span>
-          </div>
-          <div className={classes.description}>
-            <h4>Choose your meal</h4>
-            <p>Browse through many restaurants and cuisines.</p>
-          </div>
-        </div>
-        <div className={classes.step}>
-          <div className={classes.icon}>
-            <ShoppingIcon />
-            <span>3</span>
-          </div>
-          <div className={classes.description}>
-            <h4>Pay and get your food</h4>
-            <p>Pay using various payment methods.</p>
+          <div className={classes.step}>
+            <div className={classes.icon}>
+              <ShoppingIcon />
+              <span>3</span>
+            </div>
+            <div className={classes.description}>
+              <h4>Pay and get your food</h4>
+              <p>Pay using various payment methods.</p>
+            </div>
           </div>
         </div>
       </div>
