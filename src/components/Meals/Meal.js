2e5def02193e0bc38991be33bfd8d65c387063af
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-05 09:48:04 +0200

    Meals refactor

--- src/components/Meals/Meal.js
+++ src/components/Meals/Meal.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal/Modal';
+import Navbar from '../UI/Navbar/Navbar';
 import MealItemForm from './MealItem/MealItemForm';
+import classes from './Meal.module.css';
 
 const Meal = (props) => {
   const cartCtx = React.useContext(CartContext);
+
+  const price = `$${props.item.price.toFixed(2)}`;
 
   const addHandler = (amount) => {
     cartCtx.addItem({
@@ -18,7 +22,10 @@ const Meal = (props) => {
 
   return (
     <Modal onClose={props.onClose}>
-      <div>
+      <Navbar label={props.item.name} onClose={props.onClose} />
+      <div className={classes.meal}>
+        <span className={classes.price}>{price}</span>
+        <span className={classes.description}>{props.item.description}</span>
         <MealItemForm id={props.id} addHandler={addHandler} />
       </div>
     </Modal>
