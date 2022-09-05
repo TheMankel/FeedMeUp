02ce9cbecffce0134efdb67c00f969c9e9814022
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-05 09:44:26 +0200

    Input refactor

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import AddIcon from '../Icons/AddIcon';
+import RemoveIcon from '../Icons/RemoveIcon';
 
 import classes from './Input.module.css';
 
 const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
-      <label htmlFor={props.input.id}>{props.label}</label>
+      {/* <label htmlFor={props.input.id}>{props.label}</label> */}
+      <span onClick={props.onRemove}>
+        <RemoveIcon />
+      </span>
       <input ref={ref} {...props.input}></input>
+      <span onClick={props.onAdd}>
+        <AddIcon />
+      </span>
     </div>
   );
 });
