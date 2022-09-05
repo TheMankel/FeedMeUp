ab87fe25f428fb8853fbd30942cb3c02815efb35
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-05 10:14:16 +0200

    Clean up

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -7,7 +7,6 @@ import classes from './Input.module.css';
 const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
-      {/* <label htmlFor={props.input.id}>{props.label}</label> */}
       <span onClick={props.onRemove}>
         <RemoveIcon />
       </span>
