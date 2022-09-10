910abad5dab6283fd624bd2a35a6c3742dd6f9e5
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2022-09-10 10:00:51 +0200

    Added FormInput component

---
+++ src/components/UI/FormInput/FormInput.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import classes from './FormInput.module.css';
+
+const FormInput = (props) => {
+  return (
+    <div className={classes.control}>
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        type={props.type}
+        id={props.id}
+        placeholder={props.placeholder}
+        autocomplete='off'
+      />
+    </div>
+  );
+};
+
+export default FormInput;
